ecaf91b44866b6ddf89d4073edc6fe4048c11081
Dimas Adrian Mukti <57429296+berodimas@users.noreply.github.com>
2022-01-27 14:28:10 +0700

    Update notiolink.config.js

--- notiolink.config.js
+++ notiolink.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /** Application Name, will be shown on the index page */
-  appName: 'Notiolink',
+  appName: 'Notiolink-Bero',
 
   /** Description for SEO */
   seoDescription:
